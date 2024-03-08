7b0e2ec772f9943d12e394273d45511182ad0878
Panidaeiei <161023725+Panidaeiei@users.noreply.github.com>
2024-03-08 22:37:31 +0700

    first

--- app.ts
+++ app.ts
@@ -3,9 +3,14 @@ import { router as insert } from "./api/insert";
 import { router as Deletes } from "./api/delete";
 import { router as select } from "./api/select";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 export const app = express();
-
+// app.use(
+//     cors({
+//         origin:"*",
+//     })
+// )
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use("/insert", insert);
